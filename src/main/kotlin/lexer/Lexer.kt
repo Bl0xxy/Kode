9b1f3783f7190ea8aecd777ed4884632041989c8
Bl0xxy <bloxygamingstudio@gmail.com>
2026-01-26 21:06:44 -0600

    Started Lexer.kt

---
+++ src/main/kotlin/lexer/Lexer.kt
@@ -0,0 +1,63 @@
+package io.bl0xxy.lexer
+
+enum class TokenType {
+    // Symbols
+    PLUS, MINUS, MUL, DIV,
+    LPAREN, RPAREN,
+
+    // Literals
+    NUMBER, STRING,
+
+    // Other
+    IDENTIFIER,
+
+    // Special
+    EOL, EOF
+}
+
+data class Token(val type: TokenType, val value: String? = null);
+
+class Lexer(private val input: String) {
+    private val symbols = charArrayOf('+', '-', '*', '/')
+
+    private var pos = 0
+    private val len = input.length
+    private val currentChar: Char? get() = input.getOrNull(pos)
+
+    private fun advance() {
+        pos++
+    }
+
+    private fun skipWhitespace() {
+        while (currentChar?.isWhitespace() == true)
+            advance()
+    }
+
+    fun getNextToken(): Token {
+        skipWhitespace()
+
+        val current = currentChar ?: return Token(TokenType.EOF)
+
+        return when {
+            current.isDigit() -> number()
+            current.isLetter() || current == '_' -> identifier()
+            else -> throw IllegalArgumentException("Illegal character: $current");
+        }
+    }
+
+    private fun number(): Token {
+        val start = pos
+        while (currentChar?.isDigit() == true)
+            advance()
+
+        return Token(TokenType.NUMBER, input.substring(start, pos))
+    }
+
+    private fun identifier(): Token {
+        val start = pos
+        while (currentChar?.let { it.isLetterOrDigit() || it == '_' } == true)
+            advance()
+
+        return Token(TokenType.IDENTIFIER, input.substring(start, pos))
+    }
+}
