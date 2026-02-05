955f0159e1708d6a1c9b9dcb68f4d7037363a0b9
Bl0xxy <bloxygamingstudio@gmail.com>
2026-02-04 20:32:50 -0600

    Started framework for classes and custom types in Kode

--- src/main/kotlin/bytecode/Bytecode.kt
+++ src/main/kotlin/bytecode/Bytecode.kt
@@ -1,12 +1,28 @@
 package io.bl0xxy.bytecode
 
+import io.bl0xxy.metadata.FunctionMeta
+
 enum class BytecodeValueType {
     INT,
     STRING,
-    BOOLEAN
+    BOOLEAN,
+    DYNAMIC_VALUE
 }
 
-data class BytecodeValue(
-    val value: Any,
-    val type: BytecodeValueType
+data class ClassMeta(
+    val name: String,
+    val fields: Map<String, BytecodeValueType>,
+    val methods: Map<String, FunctionMeta>
 )
+
+data class ClassInstance(
+    val meta: ClassMeta,
+    val fieldValues: Map<String, BytecodeValueType> = mutableMapOf()
+)
+
+sealed class BytecodeValue(open val type: BytecodeValueType) {
+    data class IntValue(val value: Int) : BytecodeValue(BytecodeValueType.INT)
+    data class StringValue(val value: String) : BytecodeValue(BytecodeValueType.STRING)
+    data class BooleanValue(val value: Boolean) : BytecodeValue(BytecodeValueType.BOOLEAN)
+    data class ClassInstance(val value: ClassInstance?) : BytecodeValue(BytecodeValueType.DYNAMIC_VALUE) {}
+}
